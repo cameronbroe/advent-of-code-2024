e5d30615fa484b1eec32d2df61cf9959dcadfccb
Cameron Roe <cameron@cameronbroe.com>
2024-12-05 19:04:53 -0600

    day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,21 +2,15 @@ use utils::*;
 
 const INPUT_FILE: &str = "inputs/day2.txt";
 
-fn is_report_safe(report: String) -> bool {
-    let levels: Vec<&str> = report.split(" ").collect();
-    let parsed_levels: Vec<u32> = levels
-        .into_iter()
-        .map(|x| x.parse::<u32>().unwrap())
-        .collect();
-
+fn is_report_safe(report: &Vec<u32>) -> bool {
     let mut is_safe = false;
-    let is_sorted = parsed_levels.clone().iter().is_sorted_by(|x, y| x <= y)
-        || parsed_levels.clone().iter().is_sorted_by(|x, y| x >= y);
+    let is_sorted = report.clone().iter().is_sorted_by(|x, y| x <= y)
+        || report.clone().iter().is_sorted_by(|x, y| x >= y);
     if is_sorted {
         is_safe = true;
-        for i in 0..(parsed_levels.len() - 1) {
-            let curr = parsed_levels[i];
-            let next = parsed_levels[i + 1];
+        for i in 0..(report.len() - 1) {
+            let curr = report[i];
+            let next = report[i + 1];
             let diff = next.abs_diff(curr);
             if diff < 1 || diff > 3 {
                 is_safe = false;
@@ -27,11 +21,50 @@ fn is_report_safe(report: String) -> bool {
     return is_safe;
 }
 
+fn is_safe_with_dampener(report: &Vec<u32>) -> bool {
+    let mut is_safe = false;
+    for i in 0..report.len() {
+        let mut dampened_report = report.clone();
+        dampened_report.remove(i);
+        if is_report_safe(&dampened_report) {
+            is_safe = true;
+            break;
+        }
+    }
+
+    return is_safe;
+}
+
 fn day2_part1(input: String) -> String {
     let mut num_safe: u32 = 0;
     for report in input.lines() {
-        if is_report_safe(report.to_string()) {
+        let levels: Vec<&str> = report.split(" ").collect();
+        let parsed_levels: Vec<u32> = levels
+            .into_iter()
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+        if is_report_safe(&parsed_levels) {
             num_safe += 1;
+        }
+    }
+
+    return num_safe.to_string();
+}
+
+fn day2_part2(input: String) -> String {
+    let mut num_safe: u32 = 0;
+    for report in input.lines() {
+        let levels: Vec<&str> = report.split(" ").collect();
+        let parsed_levels: Vec<u32> = levels
+            .into_iter()
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+        if is_report_safe(&parsed_levels) {
+            num_safe += 1;
+        } else {
+            if is_safe_with_dampener(&parsed_levels) {
+                num_safe += 1;
+            }
         }
     }
 
@@ -42,6 +75,8 @@ fn main() {
     let input = load_input(INPUT_FILE.to_string());
     let part1_output = day2_part1(input.clone());
     println!("part 1: {}", part1_output);
+    let part2_output = day2_part2(input.clone());
+    println!("part 2: {}", part2_output);
 }
 
 #[cfg(test)]
@@ -55,5 +90,12 @@ mod tests {
         let input = load_input(SAMPLE_INPUT_FILE.to_string());
         let output = day2_part1(input);
         assert_eq!(output.to_owned(), "2");
+    }
+
+    #[test]
+    fn day2_part2_sample_passes() {
+        let input = load_input(SAMPLE_INPUT_FILE.to_string());
+        let output = day2_part2(input);
+        assert_eq!(output.to_owned(), "4");
     }
 }
